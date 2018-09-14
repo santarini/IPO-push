558e736cfac7b12207824b2edcdf9322bef777a2
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-13 15:47:50 -1000

    Update yahooScrape.py

--- yahooScrape.py
+++ yahooScrape.py
@@ -1,24 +1,66 @@
 import datetime
 import requests
 import bs4 as bs
+from datetime import datetime, timedelta
+import csv
 
-i = 0
-dateNow = datetime.datetime.now().date()
-response = requests.get('https://finance.yahoo.com/calendar/ipo?day='+ str(dateNow))
-soup = bs.BeautifulSoup(response.text, 'lxml')
-for table in soup.findAll('table'):
-    for tbody in table.findAll('tbody'):
-        for tr in table.findAll('tr'):
-            if tr.find('th'):
-                continue
-            else:
-                ticker = tr.findAll("td")[0]
-                companyName = tr.findAll("td")[1]
-                indexName = tr.findAll("td")[2]
-                ipoDate = tr.find("span")
-                price = tr.findAll("td")[3]
-                priceRange = tr.findAll("td")[4]
-                currency = tr.findAll("td")[5]
-                shares = tr.findAll("td")[6]
-                actions = tr.findAll("td")[7]
-                print(ipoDate.text)
+#figure out today's date and the first and last day of this week
+
+weekdays = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"]
+today = datetime.today().weekday()
+todayDate = datetime.today()
+beginWeek = datetime.today() - timedelta(days=(today+1))
+endWeek = beginWeek + timedelta(days=6)
+
+#make a list for all dates in this week
+datesInWeek =[]
+
+for x in range (0, 7):
+    datesInWeek.append((beginWeek + timedelta(days = x)).strftime('%Y-%m-%d'))
+
+
+with open('ipoData.csv', 'a') as csvfileB:
+    fieldnames = ['Ticker',
+                  'Company Name',
+                  'Index Name',
+                  'Date',
+                  'Price',
+                  'Price Range',
+                  'Currency',
+                  'Shares',
+                  'Actions'
+                  ]
+    writer = csv.DictWriter(csvfileB, fieldnames=fieldnames, lineterminator = '\n')
+    writer.writeheader()
+    #perform main function for each day in week
+    for x in range (0, 7):
+        response = requests.get('https://finance.yahoo.com/calendar/ipo?from=' + beginWeek.strftime("%Y-%m-%d") +' &to=' + endWeek.strftime("%Y-%m-%d") + '&day=' + datesInWeek[x] )
+        soup = bs.BeautifulSoup(response.text, 'lxml')
+        for table in soup.findAll('table'):
+            for tbody in table.findAll('tbody'):
+                for tr in table.findAll('tr'):
+                    if tr.find('th'):
+                        continue
+                    else:
+                        ticker = tr.findAll("td")[0]
+                        companyName = tr.findAll("td")[1]
+                        indexName = tr.findAll("td")[2]
+                        ipoDate = tr.find("span")
+                        price = tr.findAll("td")[4]
+                        priceRange = tr.findAll("td")[5]
+                        currency = tr.findAll("td")[6]
+                        shares = tr.findAll("td")[7]
+                        actions = tr.findAll("td")[8]
+                        
+                        writer.writerow({'Ticker': ticker.text ,
+                                         'Company Name': companyName.text,
+                                         'Index Name': indexName.text,
+                                         'Date': ipoDate.text,
+                                         'Price': price.text,
+                                         'Price Range': priceRange.text,
+                                         'Currency': currency.text,
+                                         'Shares': shares.text,
+                                         'Actions': actions.text
+                                         })
+
+print("Done.")
