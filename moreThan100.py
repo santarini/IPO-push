7e8c7b61a205fd1c05b3f2cbe9bb7b7ad59652f6
Makoa <32736507+santarini@users.noreply.github.com>
2018-09-13 17:01:06 -1000

    Update moreThan100.py

--- moreThan100.py
+++ moreThan100.py
@@ -1,3 +1,10 @@
+import datetime
+import requests
+import bs4 as bs
+from datetime import datetime, timedelta
+import csv
+import re
+
 response = requests.get('https://finance.yahoo.com/calendar/economic?from=2018-09-09&to=2018-09-15&day=2018-09-10')
 soup = bs.BeautifulSoup(response.text, 'lxml')
 resultsSpan = soup('span', text=re.compile("of \d\d\d results"))[0].text
